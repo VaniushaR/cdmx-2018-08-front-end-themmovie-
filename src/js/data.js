d99f835ab0cb5d2d027583a325fb4848e0436ffb
VaniushaR <vaniusha.ra@gmail.com>
2018-08-22 19:17:01 -0500

    Creating filters of validation for the search input

--- src/js/data.js
+++ src/js/data.js
@@ -1,14 +1,28 @@
 alert('conected');
 const urlHost = 'http://www.omdbapi.com/?s=';
-const apiKey = '&plot=full&apikey=cd8f92e5';
+const api = '&plot=full&apikey=cd8f92e5';
 let host = '';
 let search = document.getElementById('search');
 let results = '';
 
 const getMovies = () => {
   console.log(search.value);
-  search = search.value;
-  host = `${urlHost}${search}${apiKey}`;
+  const searchValue = search.value;
+  if (searchValue == '') {
+    alert('Please write some key word to search movie titles');
+    location.reload();
+  }
+  let searchArray = searchValue.split('');
+  for (i = 0; i < searchArray.length; i++) {
+    if (searchArray[0] === '') {
+      searchArray[0].push('&');
+    }
+  }
+  console.log(searchArray);
+  let searchJoin = searchArray.join('');
+  console.log(searchJoin);
+
+  host = `${urlHost}${searchJoin}${api}`;
   console.log(host);
   fetch(host)
     .then(response => response.json())
@@ -18,6 +32,7 @@ const getMovies = () => {
     .catch(error => {
       console.log(error);
     });
+  //}
 };
 
 const btnSearch = document.getElementById('btn-search');
@@ -60,11 +75,3 @@ const printerResults = data => {
   const res = document.getElementById('results');
   res.innerHTML = results;
 };
-
-//     for (let i = 0; i < x.length; i++) {
-//       result +=
-//     }
-//   };
-
-// };
-// //window.onload = host => {
