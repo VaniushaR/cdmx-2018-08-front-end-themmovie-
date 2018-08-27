708f92f16f387ee7dc0e07cb001bb16dc7f46b7b
VaniushaR <vaniusha.ra@gmail.com>
2018-08-26 20:09:10 -0500

    Adding Style & renderingg results

--- src/js/data.js
+++ src/js/data.js
@@ -1,9 +1,11 @@
-//alert('conected');
+alert('conected');
 const urlHost = 'http://www.omdbapi.com/?s=';
 const api = '&plot=full&apikey=cd8f92e5';
 let host = '';
 let search = document.getElementById('search');
+let results = '';
 
+//function that obtains the URL from the user search
 const getMovies = () => {
   console.log(search.value);
   const searchValue = search.value;
@@ -23,6 +25,11 @@ const getMovies = () => {
 
   host = `${urlHost}${searchJoin}${api}`;
   console.log(host);
+  fetchApi(host);
+  return host;
+};
+//fetch to the API
+const fetchApi = host => {
   fetch(host)
     .then(response => response.json())
     .then(data => {
@@ -34,8 +41,44 @@ const getMovies = () => {
   //}
 };
 
-// btnSearch.addEventListener('click', event => {
-//   const container = document.getElementById('container');
-//   container.innerHTML = '';
-//   getMovies();
-// });
+const btnSearch = document.getElementById('btn-search');
+btnSearch.addEventListener('click', getMovies);
+
+const printerResults = data => {
+  console.log(data);
+  let result = '';
+  data = data.Search;
+  console.log(data);
+  data.forEach(getTitles => {
+    console.log(getTitles);
+    let movieTitle = getTitles.Title;
+    console.log(movieTitle);
+    let moviePoster = getTitles.Poster;
+    console.log(moviePoster);
+    let movieType = getTitles.Type;
+    console.log(movieType);
+    let movieYear = getTitles.Year;
+    console.log(movieYear);
+    results += `
+
+         <div class="row">
+         <div class="col s12 m6 l6 xl6">
+          <div class="card">
+           <div class="card-image">
+          <img src="${moviePoster}" alt="Poster of the movie ${movieTitle}">
+          <span class="card-title movie-titles black">Título: ${movieTitle}</span>
+            </div>
+             <div class="card-content">
+             <h6>Type: ${movieType}</h6>
+             </div>
+            <div class="card-action">
+             Year: ${movieYear}
+             </div>
+           </div>
+         </div>
+       </div>
+               `;
+  });
+  const res = document.getElementById('results');
+  res.innerHTML = results;
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,47 @@
-let container;
-let results = '';
-let res = document.getElementById('results');
-const btnSearch = document.getElementById('btn-search');
+// alert('conectado-main');
+// let container;
+// let results = '';
+// let res = document.getElementById('results');
+// const btnSearch = document.getElementById('btn-search');
 
-btnSearch.addEventListener('click', getMovies);
+// btnSearch.addEventListener('click', getMovies);
 
-const printerResults = data => {
-  console.log(res);
-  console.log(data);
-  data = data.Search;
-  console.log(data);
-  data.forEach(getTitles => {
-    console.log(getTitles);
-    let movieTitle = getTitles.Title;
-    console.log(movieTitle);
-    let moviePoster = getTitles.Poster;
-    console.log(moviePoster);
-    let movieType = getTitles.Type;
-    console.log(movieType);
-    let movieYear = getTitles.Year;
-    console.log(movieYear);
-    results += `
-  
-    <div class="row">
-    <div class="col s12 m6 l6 xl6">
-      <div class="card">
-        <div class="card-image">
-        <img src="${moviePoster}" alt="Poster of the movie ${movieTitle}">
-          <span class="card-title movie-titles black">Título: ${movieTitle}</span>
-        </div>
-        <div class="card-content">
-        <h6>Type: ${movieType}</h6>
-        </div>
-        <div class="card-action">
-        Year: ${movieYear}
-        </div>
-      </div>
-    </div>
-  </div>   
-          `;
-  });
+// const printerResults = data => {
+//   console.log(res);
+//   console.log(data);
+//   data = data.Search;
+//   console.log(data);
+//   data.forEach(getTitles => {
+//     console.log(getTitles);
+//     let movieTitle = getTitles.Title;
+//     console.log(movieTitle);
+//     let moviePoster = getTitles.Poster;
+//     console.log(moviePoster);
+//     let movieType = getTitles.Type;
+//     console.log(movieType);
+//     let movieYear = getTitles.Year;
+//     console.log(movieYear);
+//     results += `
 
-  res.innerHTML = results;
-};
+//     <div class="row">
+//     <div class="col s12 m6 l6 xl6">
+//       <div class="card">
+//         <div class="card-image">
+//         <img src="${moviePoster}" alt="Poster of the movie ${movieTitle}">
+//           <span class="card-title movie-titles black">Título: ${movieTitle}</span>
+//         </div>
+//         <div class="card-content">
+//         <h6>Type: ${movieType}</h6>
+//         </div>
+//         <div class="card-action">
+//         Year: ${movieYear}
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+//           `;
+//   });
+
+//   res.innerHTML = results;
+//   alert('ended');
+// };
